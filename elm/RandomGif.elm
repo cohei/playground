8f6b4cbcd3cd15e1f4af99898baef0ace2438ac5
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-08-26 12:07:49 +0900

    Elm

---
+++ elm/RandomGif.elm
@@ -0,0 +1,56 @@
+module RandomGif exposing (main)
+
+import Html exposing (Html, div, h2, text, img, button, program)
+import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
+
+main =
+    program { init = init, update = update, subscriptions = \_ -> Sub.none, view = view }
+
+
+type alias Model =
+    { topic : String, gifUrl : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "cats" "waiting.gif", Cmd.none )
+
+
+type Msg
+    = MorePlease
+    | NewGif (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MorePlease ->
+            ( model, getRandomGif model.topic )
+        NewGif (Err _) ->
+            ( model, Cmd.none )
+        NewGif (Ok newUrl) ->
+            ( { model | gifUrl = newUrl }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text model.topic ]
+        , img [ src model.gifUrl ] []
+        , button [ onClick MorePlease ] [ text "More Please!" ]
+        ]
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+    let
+        url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+        request = Http.get url decodeGifUrl
+    in
+        Http.send NewGif request
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl = Decode.at ["data", "image_url"] Decode.string
