938198168095379049b3263638500260e23be299
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-12-02 14:13:57 +0900

    Haskell

---
+++ haskell/src/Bifold.hs
@@ -0,0 +1,32 @@
+module Bifold where
+
+import Prelude hiding (sum)
+
+{-
+a   -> a' -> ... -> a''
+    b        bs
+c'' -> c' -> ... -> c
+-}
+
+fold :: ((a, c) -> b -> (a, c)) -> (a, c) -> [b] -> (a, c)
+fold _ z      []       = z
+fold f (a, c) (b : bs) = (a'', c'')
+  where
+    (a'', c') = fold f (a', c) bs
+    (a', c'') = f (a, c') b
+
+halve :: [a] -> ([a], [a])
+halve xs = (front, rear)
+  where
+    ((rear, _), front) = fold f ((xs, xs), []) xs
+
+    f ((rear', []), _)         _ = ((rear', []), [])
+    f ((rear', count), front') x = ((tail rear', drop 2 count), x : front')
+
+diffav :: Fractional a => [a] -> [a]
+diffav []     = []
+diffav (x:xs) = x - average : ys
+  where
+    average = sum / n
+    ((sum, n), ys) = fold f ((x, 1), []) xs
+    f ((s, i), zs) z = ((s + z, i + 1), (z - average) : zs)
