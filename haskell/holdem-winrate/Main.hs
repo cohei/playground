b6a664dcb773a1873c9b05cc4ea904c0f3c3ab09
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-07-03 21:09:46 +0900

    Next: fix `isStraight`

--- haskell/holdem-winrate/Main.hs
+++ haskell/holdem-winrate/Main.hs
@@ -1,16 +1,61 @@
 module Main where
 
-import           Data.List (group)
+import           Data.List  (group, sortOn)
+import           Test.Hspec (describe, hspec, it, shouldBe)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = hspec $ do
+  describe "hand" $ do
+    it "checks straight flush, 5" $ do
+      hand (makeCard5 (spade 1) (spade 2) (spade 3) (spade 4) (spade 5)) `shouldBe` StraightFlush
+    it "checks straight flush, Ace" $ do
+      hand (makeCard5 (spade 1) (spade 13) (spade 12) (spade 11) (spade 10)) `shouldBe` StraightFlush
+    it "checks full house" $ do
+      hand (makeCard5 (spade 1) (heart 1) (club 1) (club 4) (spade 4)) `shouldBe` FullHouse
+    it "checks high card" $ do
+      hand (makeCard5 (spade 1) (heart 9) (club 8) (diamond 4) (spade 5)) `shouldBe` HighCard
 
 data Suit = Spade | Heart | Club | Diamond
   deriving (Eq)
 
+newtype Rank =
+  Rank Int
+  deriving Eq
+
+instance Ord Rank where
+  compare (Rank 1)  (Rank 1)  = EQ
+  compare (Rank 1)  _         = GT
+  compare _         (Rank 1)  = LT
+  compare (Rank n1) (Rank n2) = compare n1 n2
+
+instance Enum Rank where
+  toEnum 14 = 1
+  toEnum n  = Rank n
+
+  fromEnum (Rank n) = n
+
+instance Num Rank where
+  fromInteger n
+    | 1 <= n && n <= 13 = Rank $ fromIntegral n
+    | otherwise         = error "Invalid number"
+  (+) = undefined
+  (*) = undefined
+  (-) = undefined
+  abs = undefined
+  signum = undefined
+
 data Card =
-  Card { suit :: Suit, number :: Int }
+  Card { suit :: Suit, rank :: Rank }
   deriving (Eq)
+
+spade, heart, club, diamond :: Rank -> Card
+spade   n = Spade # n
+heart   n = Heart # n
+club    n = Club # n
+diamond n = Diamond # n
+
+(#) :: Suit -> Rank -> Card
+(#) = Card
 
 data Hand =
     HighCard
@@ -22,13 +67,20 @@ data Hand =
   | FullHouse
   | FourOfAKind
   | StraightFlush
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
 
 -- 5枚、小さい順
-newtype Card5 = Card5 [Card]
+newtype Card5 =
+  Card5 [Card]
+  deriving Eq
 
--- instance Ord Hand where
---   compare hand1 hand2 = _
+makeCard5 :: Card -> Card -> Card -> Card -> Card -> Card5
+makeCard5 c1 c2 c3 c4 c5 = Card5 $ sortOn rank [c1, c2, c3, c4, c5]
+
+-- 役を構成する部分の強弱を比べた上で残りのカードの強弱を比べる
+-- フルハウスは 3枚のほうから見る
+instance Ord Card5 where
+  compare cs1 cs2 = undefined
 
 hand :: Card5 -> Hand
 hand cards
@@ -43,37 +95,38 @@ hand cards
   | otherwise                         = HighCard
 
 isStraight :: Card5 -> Bool
-isStraight (Card5 cards) = isSequence $ map number cards
+isStraight (Card5 cards) = isSequence $ map rank cards
 
 isFlush :: Card5 -> Bool
 isFlush (Card5 cards) = allSame $ map suit cards
 
 isFourOfAKind :: Card5 -> Bool
 isFourOfAKind (Card5 cards) =
-  any ((== 4) . length) $ group $ map number cards
+  any ((== 4) . length) $ group $ map rank cards
 
 isFullHouse :: Card5 -> Bool
 isFullHouse (Card5 cards) =
   let
-    numbers = group $ map number cards
+    ranks = group $ map rank cards
   in
-    any ((== 3) . length) numbers && any ((== 2) . length) numbers
+    any ((== 3) . length) ranks && any ((== 2) . length) ranks
 
 isThreeOfAKind :: Card5 -> Bool
 isThreeOfAKind (Card5 cards) =
-  any ((== 3) . length) $ group $ map number cards
+  any ((== 3) . length) $ group $ map rank cards
 
 isTwoPair :: Card5 -> Bool
 isTwoPair (Card5 cards) =
-  (== 2) $ length $ filter ((== 2) . length) $ group $ map number cards
+  (== 2) $ length $ filter ((== 2) . length) $ group $ map rank cards
 
 isOnePair :: Card5 -> Bool
 isOnePair (Card5 cards) =
-  any ((== 2) . length) $ group $ map number cards
+  any ((== 2) . length) $ group $ map rank cards
 
 allSame :: Eq a => [a] -> Bool
 allSame []     = True
 allSame (x:xs) = all (x ==) xs
 
-isSequence :: (Eq a, Num a) => [a] -> Bool
-isSequence xs = all (1 ==) $ zipWith (-) (tail xs) xs
+-- ここでだけ 1 は一番小さい数にもなるので、ここでだけ特別扱いする
+isSequence :: (Eq a, Enum a) => [a] -> Bool
+isSequence xs@(x:_) = take (length xs) [x..] == xs
