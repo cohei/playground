2b1a1525e32ac04b5cb8e44d1fd8d5588d33b0b6
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-09-24 01:21:32 +0900

    Haskell: hpack run by cabal
    https://haskell.e-bigmoon.com/posts/2018/12-25-cabal-preprocessing.html

--- haskell/Setup.hs
+++ haskell/Setup.hs
@@ -1,2 +1,18 @@
-import Distribution.Simple
-main = defaultMain
+module Main (main) where
+
+import           Distribution.Simple                (Args, defaultMainWithHooks,
+                                                     preBuild, simpleUserHooks)
+import           Distribution.Simple.Setup          (BuildFlags)
+import           Distribution.Types.HookedBuildInfo (HookedBuildInfo,
+                                                     emptyHookedBuildInfo)
+import           Hpack                              (Verbose (Verbose),
+                                                     defaultOptions, hpack)
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks { preBuild = pbHpack }
+
+pbHpack :: Args -> BuildFlags -> IO HookedBuildInfo
+pbHpack _ _ = emptyHookedBuildInfo <$ hpack'
+
+hpack' :: IO ()
+hpack' = hpack Verbose defaultOptions
