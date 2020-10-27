7c643e633aa67374d723b71cb8fdb11490366a8c
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2020-10-28 03:35:29 +0900

    Haskell

--- haskell/src/FizzBuzz/Rule.hs
+++ haskell/src/FizzBuzz/Rule.hs
@@ -1,12 +1,14 @@
 module FizzBuzz.Rule (Rule, runRule, rule, modRule) where
 
+import           Control.Monad (guard)
+
 type Rule a = Int -> Maybe a
 
 runRule :: Rule a -> [Either Int a]
 runRule r = map (\i -> maybeToRight i $ r i) [1..]
 
 rule :: (Int -> Bool) -> a -> Rule a
-rule p x i = if p i then Just x else Nothing
+rule p x i = x <$ guard (p i)
 
 modRule :: Int -> a -> Rule a
 modRule n = rule (`divisible` n)
