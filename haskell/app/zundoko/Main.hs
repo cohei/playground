e3f9fd134e9775d5522e8e44bf402697b5c0bd07
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2024-03-03 00:43:57 +0900

    Haskell: format zundoko

--- haskell/app/zundoko/Main.hs
+++ haskell/app/zundoko/Main.hs
@@ -1,16 +1,14 @@
 module Main (main) where
 
-import           Control.Concurrent   (threadDelay)
-import           Control.Monad        (replicateM, (<=<))
-import           Control.Monad.Random (MonadRandom (getRandoms),
-                                       Random (random, randomR))
-import           Data.Bifunctor       (first)
-import           Data.Function        (on)
-import           Data.List            (inits, intersperse, isSuffixOf)
-import           Data.Maybe           (listToMaybe)
-import           System.Environment   (getArgs)
-import           System.IO            (BufferMode (NoBuffering), hSetBuffering,
-                                       stdout)
+import Control.Concurrent (threadDelay)
+import Control.Monad (replicateM, (<=<))
+import Control.Monad.Random (MonadRandom (getRandoms), Random (random, randomR))
+import Data.Bifunctor (first)
+import Data.Function (on)
+import Data.List (inits, intersperse, isSuffixOf)
+import Data.Maybe (listToMaybe)
+import System.Environment (getArgs)
+import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
 
 main :: IO ()
 main = maybe zunDokoKiyoshi (print <=< averageZunDokoLength) =<< readArgument
@@ -35,21 +33,21 @@ printZunDokoKiyoshi zds = do
 data ZunDoko = Zun | Doko deriving (Eq)
 
 instance Show ZunDoko where
-  show Zun  = "ズン"
+  show Zun = "ズン"
   show Doko = "ドコ"
 
 instance Random ZunDoko where
   randomR = undefined
   random = first (\b -> if b then Zun else Doko) . random
 
-zunDokos :: MonadRandom m => m [ZunDoko]
+zunDokos :: (MonadRandom m) => m [ZunDoko]
 zunDokos = takeUntil [Zun, Zun, Zun, Zun, Doko] <$> getRandoms
 
-takeUntil :: Eq a => [a] -> [a] -> [a]
+takeUntil :: (Eq a) => [a] -> [a] -> [a]
 takeUntil p = head . filter (isSuffixOf p) . inits
 
 -- expected length is about 32? want to prove
-averageZunDokoLength :: MonadRandom m => Int -> m Double
+averageZunDokoLength :: (MonadRandom m) => Int -> m Double
 averageZunDokoLength n = average . map length <$> replicateM n zunDokos
 
 average :: [Int] -> Double
