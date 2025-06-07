2b44f6527875e0a53f41cfd5989ef699b7f787cb
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2025-06-07 21:36:04 +0900

    Haskell: TypeLevelTrim

---
+++ haskell/src/TypeLevelTrim.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{- |
+[TypeScript の型のみで世界一型の厳しいプログラミング](https://zenn.dev/ame_x/articles/8ec1ec35cdc392)
+-}
+module TypeLevelTrim (Trim) where
+
+import GHC.TypeLits (Symbol, ConsSymbol, UnconsSymbol)
+
+-- >>> :kind! Trim " hello "
+-- Trim " hello " :: Symbol
+-- = "hello"
+type Trim :: Symbol -> Symbol
+type family Trim s where
+  Trim s = TrimLeft (TrimRight s)
+
+type TrimLeft :: Symbol -> Symbol
+type family TrimLeft s where
+  TrimLeft s = TrimLeft1 (UnconsSymbol s)
+
+type TrimLeft1 :: Maybe (Char, Symbol) -> Symbol
+type family TrimLeft1 unconsed where
+  TrimLeft1 Nothing = ""
+  TrimLeft1 (Just '( ' ', s )) = TrimLeft s
+  TrimLeft1 (Just '( c, s )) = ConsSymbol c s
+
+type TrimRight :: Symbol -> Symbol
+type family TrimRight s where
+  TrimRight s = Reverse (TrimLeft (Reverse s))
+
+type Reverse :: Symbol -> Symbol
+type family Reverse s where
+  Reverse s = Reverse1 "" (UnconsSymbol s)
+
+type Reverse1 :: Symbol -> Maybe (Char, Symbol) -> Symbol
+type family Reverse1 acc unconsed where
+  Reverse1 acc Nothing = acc
+  Reverse1 acc (Just '(c, s)) = Reverse1 (ConsSymbol c acc) (UnconsSymbol s)
