ca302d916add793c7f1af317b8f3026e9ffa81f8
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2026-07-19 10:10:53 +0900

    Haskell: FizzBuzz

--- haskell/src/FizzBuzz/Rule.hs
+++ haskell/src/FizzBuzz/Rule.hs
@@ -4,15 +4,22 @@ module FizzBuzz.Rule (Rule, runRule, rule, modRule) where
 
 import Control.Monad (guard)
 import Numeric.Natural (Natural)
+import Data.Coerce (coerce)
 
-newtype Rule a = Rule (Natural -> Maybe a)
-  deriving newtype (Semigroup)
+type Rule a = Natural -> Fallback Natural a
+
+newtype Fallback a b = Fallback (Either a b)
+
+instance Semigroup b => Semigroup (Fallback a b) where
+  Fallback (Left _) <> v = v
+  u <> Fallback (Left _) = u
+  Fallback (Right x) <> Fallback (Right y) = Fallback (Right (x <> y))
 
 runRule :: (Show a) => Rule a -> [String]
-runRule (Rule r) = map (\n -> maybe (show n) show (r n)) [1 ..]
+runRule r = map (either show show . coerce . r) [1 ..]
 
 rule :: (Natural -> Bool) -> a -> Rule a
-rule p x = Rule $ \n -> x <$ guard (p n)
+rule p x n = Fallback $ if p n then Right x else Left n
 
 modRule :: Natural -> a -> Rule a
 modRule n = rule (`isDivisibleBy` n)
